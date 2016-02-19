7207fd2f7f1f13d95c1e82435de06c02f3e98988
aa14h507 <aa14h507@ai.u-hyogo.ac.jp>
2016-02-19 23:09:58 +0900

    implemented methods for algebra

--- src/jp/mylib/science/common/BasicAlgebra.java
+++ src/jp/mylib/science/common/BasicAlgebra.java
@@ -66,14 +66,43 @@ public class BasicAlgebra
         return matrixT;
     }
 
+    public static double[][] generateDiagonalMatrix(double[] array)
+    {
+        double[][] diagMatrix = new double[array.length][array.length];
+        for(int i=0;i<diagMatrix.length;i++)
+            for(int j=0;j<diagMatrix[0].length;j++)
+                diagMatrix[i][j] = (i == j)? array[i] : 0.0d;
+
+        return diagMatrix;
+    }
+
     public static double[][] generateIdentityMatrix(int size)
     {
-        double[][] matrix = new double[size][size];
-        for(int i=0;i<matrix.length;i++)
-            for(int j=0;j<matrix[0].length;j++)
-                matrix[i][j] = (i == j)? 1.0d : 0.0d;
+        double[] array = new double[size];
+        for(int i=0;i<array.length;i++)
+            array[i] = 1.0d;
 
-        return matrix;
+        return generateDiagonalMatrix(array);
+    }
+
+    public static double[][] get2dRotationMatrix(double rad)
+    {
+        return new double[][]{{Math.cos(rad), -Math.sin(rad)}, {Math.sin(rad), Math.cos(rad)}};
+    }
+
+    public static double[][] get3dxRotationMatrix(double rad)
+    {
+        return new double[][]{{1.0d, 0.0d, 0.0d}, {0.0d, Math.cos(rad), -Math.sin(rad)}, {0.0d, Math.sin(rad), Math.cos(rad)}};
+    }
+
+    public static double[][] get3dyRotationMatrix(double rad)
+    {
+        return new double[][]{{Math.cos(rad), 0.0d, Math.sin(rad)}, {0.0d, 1.0d, 0.0d}, {-Math.sin(rad), 0.0d, Math.cos(rad)}};
+    }
+
+    public static double[][] get3dzRotationMatrix(double rad)
+    {
+        return new double[][]{{Math.cos(rad), -Math.sin(rad), 0.0d}, {Math.sin(rad), Math.cos(rad), 0.0d}, {0.0d, 0.0d, 1.0d}};
     }
 
     public static double calcDeterminant(double[][] matrix)
@@ -82,7 +111,6 @@ public class BasicAlgebra
         if(matrix.length != matrix[0].length)
             return det;
 
-        double prod = 1.0d;
         if(matrix.length == 2 && matrix[0].length == 2)
         {
             det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
@@ -91,7 +119,19 @@ public class BasicAlgebra
 
         for(int i=0;i<matrix.length;i++)
         {
-            // calc det in a recursive way calcDeterminant(part of "matrix");
+            double[][] subMatrix = new double[matrix.length - 1][matrix.length - 1];
+            int l = 0;
+            for(int j=0;j<matrix.length;j++)
+                if(i != j)
+                {
+                    for(int k=1;k<matrix.length;k++)
+                        subMatrix[l][k - 1] = matrix[j][k];
+
+                    l++;
+                }
+
+            double sign = (i % 2 == 0)? 1.0d : -1.0d;
+            det += sign * matrix[i][0] * calcDeterminant(subMatrix);
         }
 
         return det;
@@ -99,15 +139,38 @@ public class BasicAlgebra
 
     public static double[][] calcInverseMatrix(double[][] matrix)
     {
-        double[][] invMatrix = new double[matrix.length][matrix.length];
         if(matrix.length != matrix[0].length)
-            return invMatrix;
+            return new double[matrix.length][matrix.length];
 
         double det = calcDeterminant(matrix);
         if(det == 0.0d)
-            return invMatrix;
+            return new double[matrix.length][matrix.length];
 
-        // adjugate matrix
+        // sweep out method
+        double[][] invMatrix = generateIdentityMatrix(matrix.length);
+        double buf = 0.0d;
+        for(int i=0;i<matrix.length;i++)
+        {
+            buf = 1.0d / matrix[i][i];
+            for(int j=0;j<matrix.length;j++)
+            {
+                matrix[i][j] *= buf;
+                invMatrix[i][j] *= buf;
+            }
+
+            for(int j=0;j<matrix.length;j++)
+            {
+                if(i != j)
+                {
+                    buf = matrix[j][i];
+                    for(int k=0;k<matrix.length;k++)
+                    {
+                        matrix[j][k] -= matrix[i][k] * buf;
+                        invMatrix[j][k] -= invMatrix[i][k] * buf;
+                    }
+                }
+            }
+        }
 
         return invMatrix;
     }
